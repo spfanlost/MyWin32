8686960518a0d915e97d20edd7fc726f595b1534
imyumeng <imyumeng@qq.com>
2020-05-25 22:49:31 +0800

    add listview ctrl

--- main.cpp
+++ main.cpp
@@ -9,12 +9,11 @@
  */
 
 /* Includes ------------------------------------------------------------------*/
-#include <windows.h>
-#include <tchar.h>
-#include <commctrl.h>
-#include "resource.h"
 #include "comm_def.h"
 #include "main.h"
+#include <windows.h>
+#include <commctrl.h>
+#include "resource.h"
 
 /* Private declaration -------------------------------------------------------*/
 /* Extern variables declaration ----------------------------------------------*/
@@ -23,12 +22,18 @@
 /* Local functions definition ------------------------------------------------*/
 
 HICON hDlgIcon;
-
+int iSelect=0;
+int flag=0;
+HWND hEdit;
+LVITEM LvItem;  // ListView Item struct
+bool escKey=0;
+char tempstr[100]="";
+TCHAR tchar;
+MSG msg;
 char tmpBuf[128];
 
 void FindFile(const std::string strPath)
 {
-
     WIN32_FIND_DATAA findData = {0};
     const std::string strFindPath = strPath + "\\*.*";
     HANDLE hFindFine = FindFirstFileA(strFindPath.c_str(), &findData);
@@ -56,24 +61,91 @@ GoOut:
     FindClose(hFindFine);
 }
 
-void InsertListviewItem(HWND hList, int nPos, LPSTR wszFirst, LPSTR wszSecond, LPSTR wszThrid)
+LRESULT
+ProcessCustomDraw(LPARAM lParam)
+{
+    LPNMLVCUSTOMDRAW lplvcd = (LPNMLVCUSTOMDRAW)lParam;
+    switch (lplvcd->nmcd.dwDrawStage)
+    {
+        case CDDS_PREPAINT: // Before the paint cycle begins
+            return CDRF_NOTIFYITEMDRAW;// request notifications for individual listview items
+        case CDDS_ITEMPREPAINT: // Before an item is drawn
+            return CDRF_NOTIFYSUBITEMDRAW;
+
+        case CDDS_SUBITEM | CDDS_ITEMPREPAINT: // Before a subitem is drawn
+        {
+            switch (lplvcd->iSubItem)
+            {
+                case 0:
+                {
+                    lplvcd->clrText = RGB(255, 255, 255);
+                    lplvcd->clrTextBk = RGB(240, 55, 23);
+                    return CDRF_NEWFONT;
+                }
+                case 1:
+                {
+                    lplvcd->clrText = RGB(255, 255, 0);
+                    lplvcd->clrTextBk = RGB(0, 0, 0);
+                    return CDRF_NEWFONT;
+                }
+                case 2:
+                {
+                    lplvcd->clrText = RGB(20, 26, 158);
+                    lplvcd->clrTextBk = RGB(200, 200, 10);
+                    return CDRF_NEWFONT;
+                }
+                case 3:
+                {
+                    lplvcd->clrText = RGB(12, 15, 46);
+                    lplvcd->clrTextBk = RGB(200, 200, 200);
+                    return CDRF_NEWFONT;
+                }
+                case 4:
+                {
+                    lplvcd->clrText = RGB(120, 0, 128);
+                    lplvcd->clrTextBk = RGB(20, 200, 200);
+                    return CDRF_NEWFONT;
+                }
+                case 5:
+                {
+                    lplvcd->clrText = RGB(255, 255, 255);
+                    lplvcd->clrTextBk = RGB(0, 0, 150);
+                    return CDRF_NEWFONT;
+                }
+            }
+            break;
+        }
+    }
+    return CDRF_DODEFAULT;
+}
+
+void InsertListviewFirstItem(HWND hList,
+                            int nPos,
+                            LPSTR pszText)
 {
     LVITEM lvi;
+    memset(&lvi,0,sizeof(lvi));
     lvi.mask = LVIF_TEXT | LVIF_IMAGE;
     lvi.iImage = 0;
-    lvi.pszText = (LPSTR) "";
-    lvi.cchTextMax = 1024;
-
+    lvi.cchTextMax = 256;
     lvi.iItem = nPos;
-    lvi.iSubItem = 0;
+    lvi.iSubItem=0; // Put in first coluom
+    lvi.pszText=pszText; // Text to display (can be from a char variable) (Items)
     ListView_InsertItem(hList, &lvi);
-    ListView_SetItemText(hList, nPos, 0, wszFirst);
-    lvi.iSubItem = 1;
-    ListView_SetItemText(hList, nPos, 1, wszSecond);
-    lvi.iSubItem = 2;
-    ListView_SetItemText(hList, nPos, 2, wszThrid);
-    return;
 }
+
+//sprintf(Temp,(LPSTR)"sub%d",i);
+void InsertListviewSubItem(HWND hList,
+                        int nPos,
+                        int subCnt, //1base
+                        LPSTR pszText)
+{
+    for(int i=1; i<=subCnt; i++) // Add SubItems in a loop
+    {
+        ListView_SetItemText(hList, nPos, i, pszText);
+    }
+}
+
 
 void CreateListView(HWND hwndDlg, HWND hList)
 {
@@ -83,35 +155,45 @@ void CreateListView(HWND hwndDlg, HWND hList)
 
     // HMODULE hShell = LoadLibraryA("shell32.dll");
     // HICON hIcon = LoadIcon(hShell, MAKEINTRESOURCE(17));
-    // HIMAGELIST hImageList = ImageList_Create(GetSystemMetrics(SM_CXSMICON), GetSystemMetrics(SM_CYSMICON), ILC_MASK, 1, 0);
+    // HIMAGELIST hImageList = ImageList_Create(GetSystemMetrics(SM_CXSMICON),
+    // GetSystemMetrics(SM_CYSMICON), ILC_MASK, 1, 0);
     // ImageList_AddIcon(hImageList, hIcon);
-    // ListView_SetImageList(hList, hImageList, LVSIL_SMALL); //lStyle = LV_VIEW_SAMLLICON or LV_VIEW_TILE ThiPara LVSIL_NORMAL
+    // ListView_SetImageList(hList, hImageList, LVSIL_SMALL); //lStyle =
+    // LV_VIEW_SAMLLICON or LV_VIEW_TILE ThiPara LVSIL_NORMAL
     LVCOLUMN col;
     col.mask = LVCF_TEXT | LVCF_WIDTH | LVCF_SUBITEM;
-    col.cx = 45;
-    col.iSubItem = 0;
+    col.cx = 40;
     col.pszText = (LPSTR) "ID";
-
     ListView_InsertColumn(hList, 0, &col);
-    col.cx = 82;
-    col.iSubItem = 0;
-    col.pszText = (LPSTR) "SecTest";
+    col.cx = 50;
+    col.pszText = (LPSTR) "One";
     ListView_InsertColumn(hList, 1, &col);
-
-    col.cx = 82;
-    col.iSubItem = 0;
-    col.pszText = (LPSTR) "ThiTest";
+    col.cx = 50;
+    col.pszText = (LPSTR) "Two";
     ListView_InsertColumn(hList, 2, &col);
-    for (int i = 0; i < 20; i++)
+    col.cx = 50;
+    col.pszText = (LPSTR) "Thi";
+    ListView_InsertColumn(hList, 3, &col);
+    col.cx = 50;
+    col.pszText = (LPSTR) "For";
+    ListView_InsertColumn(hList, 4, &col);
+    col.cx = 50;
+    col.pszText = (LPSTR) "Fiv";
+    ListView_InsertColumn(hList, 5, &col);
+    for (int i=0, j = 0; i < 2; i++)
     {
         sprintf(tmp1, "%d", i + 1);
-        sprintf(tmp2, "test%d", i);
-        sprintf(tmp3, "TEST%d", i);
-        InsertListviewItem(hList, i, (LPSTR)tmp1, (LPSTR)tmp2, (LPSTR)tmp3);
+        InsertListviewFirstItem(hList,i,(LPSTR)tmp1);
+        for(int sub=1; sub<=5; sub++)
+        {
+            sprintf(tmp1,(LPSTR)"sub%d",sub);
+            InsertListviewSubItem(hList,i,sub,tmp1);
+        }
     }
     LONG lStyle = GetWindowLong(hList, GWL_STYLE);
     lStyle &= ~LVS_TYPEMASK;
-    lStyle |= LV_VIEW_DETAILS; //LV_VIEW_DETAILS LV_VIEW_ICON LV_VIEW_SMALLICON LV_VIEW_LIST LV_VIEW_TILE
+    lStyle |= LV_VIEW_DETAILS; // LV_VIEW_DETAILS LV_VIEW_ICON LV_VIEW_SMALLICON
+        // LV_VIEW_LIST LV_VIEW_TILE
     ListView_SetExtendedListViewStyle(hList, LVS_EX_FULLROWSELECT);
     SetWindowLong(hList, GWL_STYLE, lStyle);
 }
@@ -122,7 +204,12 @@ void ReadTnifileStr(LPCTSTR Section, LPCTSTR Key, LPTSTR Buf, LPCTSTR IniName)
     assert(NULL != Key);
     assert(NULL != Buf);
     assert(NULL != IniName);
-    GetPrivateProfileString(Section, Key, (LPCTSTR) "Read init didn't work!", Buf, sizeof(Buf), IniName);
+    GetPrivateProfileString(Section,
+                            Key,
+                            (LPCTSTR) "Read init didn't work!",
+                            Buf,
+                            sizeof(Buf),
+                            IniName);
 }
 
 void StayOnTop(HWND hwndDlg)
@@ -141,6 +228,7 @@ void StayOnTop(HWND hwndDlg)
         SetWindowPos(hwndDlg, HWND_NOTOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE);
     }
 }
+
 void ListBoxCtrl(HWND hwndDlg, WPARAM wParam)
 {
     switch (LOWORD(wParam))
@@ -164,8 +252,13 @@ void ListBoxCtrl(HWND hwndDlg, WPARAM wParam)
                 GetDlgItemText(hwndDlg, IDC_EDIT_AddStr, buf, len + 1);
                 for (i = 0; i < nTimes; i++)
                 {
-                    int index = SendDlgItemMessage(hwndDlg, IDC_LISTBOX, LB_ADDSTRING, 0, (LPARAM)buf);
-                    SendDlgItemMessage(hwndDlg, IDC_LISTBOX, LB_SETITEMDATA, (WPARAM)index, (LPARAM)nTimes);
+                    int index = SendDlgItemMessage(
+                        hwndDlg, IDC_LISTBOX, LB_ADDSTRING, 0, (LPARAM)buf);
+                    SendDlgItemMessage(hwndDlg,
+                                       IDC_LISTBOX,
+                                       LB_SETITEMDATA,
+                                       (WPARAM)index,
+                                       (LPARAM)nTimes);
                 }
                 GlobalFree((HANDLE)buf);
             }
@@ -176,7 +269,8 @@ void ListBoxCtrl(HWND hwndDlg, WPARAM wParam)
         }
         else
         {
-            MessageBox(hwndDlg, "Couldn't translate that number :(", "Warning", MB_OK);
+            MessageBox(
+                hwndDlg, "Couldn't translate that number :(", "Warning", MB_OK);
         }
         break;
     }
@@ -228,7 +322,8 @@ void ListBoxCtrl(HWND hwndDlg, WPARAM wParam)
                     }
                     else
                     {
-                        MessageBox(hwndDlg, "Error getting selected item :(", "Warning", MB_OK);
+                        MessageBox(
+                            hwndDlg, "Error getting selected item :(", "Warning", MB_OK);
                     }
                 }
                 else
@@ -249,7 +344,105 @@ void ListBoxCtrl(HWND hwndDlg, WPARAM wParam)
     }
     }
 }
-BOOL CALLBACK DlgMain(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
+
+BOOL ListViewNotify(HWND hwndDlg, WPARAM wParam, LPARAM lParam)
+{
+    HWND hListview = GetDlgItem(hwndDlg, IDC_LISTVIEW); // get the ID of the ListView
+    LPNMLISTVIEW pnm = (LPNMLISTVIEW)lParam;
+    if(pnm->hdr.hwndFrom == hListview &&pnm->hdr.code == NM_CUSTOMDRAW)
+    {
+        SetWindowLong(hwndDlg, 0/*DWL_MSGRESULT*/, (LONG)ProcessCustomDraw(lParam));
+        return TRUE;
+    }
+
+    if(((LPNMHDR)lParam)->code == NM_DBLCLK)
+    {
+        char Text[255]= {0};
+        char Temp[255]= {0};
+        char Temp1[255]= {0};
+        int iSlected=0;
+        int j=0;
+
+        iSlected=SendMessage(hListview,LVM_GETNEXTITEM,-1,LVNI_FOCUSED);
+
+        if(iSlected==-1)
+        {
+            MessageBox(hwndDlg,"No Items in ListView","Error",MB_OK|MB_ICONINFORMATION);
+            return TRUE;
+        }
+
+        memset(&LvItem,0,sizeof(LvItem));
+        LvItem.mask=LVIF_TEXT;
+        LvItem.iSubItem=0;
+        LvItem.pszText=Text;
+        LvItem.cchTextMax=256;
+        LvItem.iItem=iSlected;
+
+        SendMessage(hListview,LVM_GETITEMTEXT, iSlected, (LPARAM)&LvItem);
+
+        sprintf(Temp1,Text);
+
+        for(j=1; j<=5; j++)
+        {
+            LvItem.iSubItem=j;
+            SendMessage(hListview,LVM_GETITEMTEXT, iSlected, (LPARAM)&LvItem);
+            sprintf(Temp," %s",Text);
+            lstrcat(Temp1,Temp);
+        }
+
+        MessageBox(hwndDlg,Temp1,"test",MB_OK);
+
+    }
+    if(((LPNMHDR)lParam)->code == NM_CLICK)
+    {
+        iSelect=SendMessage(hListview,LVM_GETNEXTITEM,-1,LVNI_FOCUSED);
+
+        if(iSelect==-1)
+        {
+            return TRUE;
+        }
+        flag=1;
+    }
+
+    if(((LPNMHDR)lParam)->code == LVN_BEGINLABELEDIT)
+    {
+        //Editing=1;
+        hEdit=ListView_GetEditControl(hListview);
+        GetWindowText(hEdit, tempstr, sizeof(tempstr));
+    }
+
+    if(((LPNMHDR)lParam)->code == LVN_ENDLABELEDIT)
+    {
+        int iIndex;
+        char text[255]="";
+
+        tchar = (TCHAR)msg.wParam;
+        if(tchar == 0x1b)
+            escKey=1;
+
+        iIndex=SendMessage(hListview,LVM_GETNEXTITEM,-1,LVNI_FOCUSED);
+        if(iIndex==-1)
+            return TRUE;
+
+        LvItem.iSubItem=0;
+
+        if(escKey==0)
+        {
+            LvItem.pszText=text;
+            GetWindowText(hEdit, text, sizeof(text));
+            SendMessage(hListview,LVM_SETITEMTEXT,(WPARAM)iIndex,(LPARAM)&LvItem);
+        }
+        else
+        {
+            LvItem.pszText=tempstr;
+            SendMessage(hListview,LVM_SETITEMTEXT,(WPARAM)iIndex,(LPARAM)&LvItem);
+            escKey=0;
+        }
+        //Editing=0;
+    }
+}
+BOOL CALLBACK
+DlgMain(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
     switch (uMsg)
     {
@@ -260,13 +453,25 @@ BOOL CALLBACK DlgMain(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
 
         RECT rect;
         GetWindowRect(hwndDlg, &rect);
-        LOG_DBG("rect l:%ld t:%ld r:%ld b:%ld", rect.left, rect.top, rect.right, rect.bottom);
+        LOG_DBG("rect l:%ld t:%ld r:%ld b:%ld",
+                rect.left,
+                rect.top,
+                rect.right,
+                rect.bottom);
 
-        WritePrivateProfileString(INI_SECTION1, INI_KEY2_NAME, (LPCTSTR) "55", INI_NAME);
+        WritePrivateProfileString(
+            INI_SECTION1, INI_KEY2_NAME, (LPCTSTR) "55", INI_NAME);
         memset(tmpBuf, 0, sizeof(tmpBuf));
-        GetPrivateProfileString(INI_SECTION1, INI_KEY1_STAYONTOP, (LPCTSTR) "Get didn't work", (LPTSTR)tmpBuf, 80, INI_NAME);
+        GetPrivateProfileString(INI_SECTION1,
+                                INI_KEY1_STAYONTOP,
+                                (LPCTSTR) "Get didn't work",
+                                (LPTSTR)tmpBuf,
+                                80,
+                                INI_NAME);
         LOG_DBG("%s", tmpBuf);
-        LOG_DBG(INI_SECTION1 INI_KEY2_NAME ":%d", GetPrivateProfileInt(INI_SECTION1, INI_KEY2_NAME, INI_DEF_VAL, INI_NAME));
+        LOG_DBG(INI_SECTION1 INI_KEY2_NAME ":%d",
+                GetPrivateProfileInt(
+                    INI_SECTION1, INI_KEY2_NAME, INI_DEF_VAL, INI_NAME));
 
         SetDlgItemText(hwndDlg, IDC_STATIC1, (LPCTSTR) "INITDIALOG");
 
@@ -276,21 +481,27 @@ BOOL CALLBACK DlgMain(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
         SendMessage(hProgressBar, PBM_SETRANGE, 0, MAKELPARAM(0, 100));
         SendMessage(hProgressBar, PBM_SETSTEP, (WPARAM)1, 0);
 
-        HWND hListview = GetDlgItem(hwndDlg, IDC_LISTVIEW_CTRL1); // get the ID of the ListView
+        HWND hListview = GetDlgItem(hwndDlg, IDC_LISTVIEW); // get the ID of the ListView
         CreateListView(hwndDlg, hListview);
 
         SetDlgItemText(hwndDlg, IDC_EDIT_AddStr, "Hello world!");
         SetDlgItemInt(hwndDlg, IDC_EDIT_Times, 2, FALSE);
         return TRUE;
     }
+    case WM_NOTIFY:
+        ListViewNotify(hwndDlg, wParam,lParam);
+        return TRUE;
+    case IDC_LISTVIEW:
+        return TRUE;
     case WM_CLOSE:
     {
         // RECT rect;
         // GetWindowRect(hwndDlg,&rect);
-        //WritePrivateProfileString(INI_SECTION1, INI_KEY1_STAYONTOP, rect.left, INI_NAME);
+        // WritePrivateProfileString(INI_SECTION1, INI_KEY1_STAYONTOP, rect.left,
+        // INI_NAME);
         EndDialog(hwndDlg, 0);
-        return TRUE;
     }
+        return TRUE;
     case WM_COMMAND:
     {
         switch (LOWORD(wParam))
@@ -301,14 +512,20 @@ BOOL CALLBACK DlgMain(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
         case IDC_BUTN_L:
         {
             HWND hChk2 = GetDlgItem(hwndDlg, IDC_CHK2);
-            SendMessage(hChk2, BM_SETCHECK, (WPARAM)!SendMessage(hChk2, BM_GETCHECK, 0, 0), 0);
+            SendMessage(hChk2,
+                        BM_SETCHECK,
+                        (WPARAM)!SendMessage(hChk2, BM_GETCHECK, 0, 0),
+                        0);
             LOG_DBG("IDC_BUTN_L");
             break;
         }
         case IDC_BUTN_R:
         {
             HWND hRado1 = GetDlgItem(hwndDlg, IDC_RADO1);
-            SendMessage(hRado1, BM_SETCHECK, (WPARAM)!SendMessage(hRado1, BM_GETCHECK, 0, 0), 0);
+            SendMessage(hRado1,
+                        BM_SETCHECK,
+                        (WPARAM)!SendMessage(hRado1, BM_GETCHECK, 0, 0),
+                        0);
             LOG_DBG("IDC_BUTN_R");
             break;
         }
@@ -316,10 +533,48 @@ BOOL CALLBACK DlgMain(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
             LOG_DBG("IDC_RADO1");
             break;
         case IDC_LISTBOX:
+            LOG_DBG("LISTBOX");
         case IDC_BUTN_ListBoxAdd:
         case IDC_BUTN_ListBoxRemove:
         case IDC_BUTN_ListBoxClear:
             ListBoxCtrl(hwndDlg, wParam);
+            break;
+        case IDC_BUTN_Listview_AddItem:
+            {
+                int iItem;
+                char ItemText[100];
+                HWND hListview = GetDlgItem(hwndDlg, IDC_LISTVIEW); // get the ID of the ListView
+                iItem=SendMessage(hListview,LVM_GETITEMCOUNT,0,0);
+                GetDlgItemText(hwndDlg,IDC_EDIT_ADD,ItemText,100);
+                if((lstrlen(ItemText))==0)
+                {
+                    MessageBox(hwndDlg,"Please Write Some Text","Error",MB_OK|MB_ICONINFORMATION);
+                    break;
+                }
+                InsertListviewFirstItem(hListview,iItem,ItemText);
+            }
+            LOG_DBG(TEST_FAIL);
+            break;
+        case IDC_BUTN_Listview_AddSubItem:
+            {
+                int Item,i;
+                char SubItemText[100];
+                HWND hListview = GetDlgItem(hwndDlg, IDC_LISTVIEW); // get the ID of the ListView
+                Item=SendMessage(hListview,LVM_GETITEMCOUNT,0,0);
+
+                GetDlgItemText(hwndDlg,IDC_EDIT_ADDSUB,SubItemText,100);
+
+                if((lstrlen(SubItemText))==0)
+                {
+                    MessageBox(hwndDlg,"Please Write Some Text","Error",MB_OK|MB_ICONINFORMATION);
+                    break;
+                }
+                for(int sub=1; sub<=5; sub++)
+                {
+                    InsertListviewSubItem(hListview,Item-1,sub,SubItemText);
+                }
+            }
+            LOG_DBG(TEST_PASS);
             break;
         }
         return TRUE;
@@ -345,10 +600,15 @@ BOOL CALLBACK DlgMain(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
     return FALSE;
 }
 
-int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
+int APIENTRY
+WinMain(HINSTANCE hInstance,
+        HINSTANCE hPrevInstance,
+        LPSTR lpCmdLine,
+        int nShowCmd)
 {
     InitCommonControls();
     hDlgIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ICON1));
-    //FindFile("D:\\Documents\\_Project_\\git\\myWin32");
-    return DialogBox(hInstance, MAKEINTRESOURCE(DLG_MAIN), NULL, (DLGPROC)DlgMain);
+    // FindFile("D:\\Documents\\_Project_\\git\\myWin32");
+    return DialogBox(
+        hInstance, MAKEINTRESOURCE(DLG_MAIN), NULL, (DLGPROC)DlgMain);
 }
--- resource.h
+++ resource.h
@@ -7,7 +7,7 @@
 #define IDC_RADO1                               40000
 #define IDC_BUTN_L                              40001
 #define IDC_BUTN_R                              40002
-#define IDC_LISTVIEW_CTRL1                      40003
+#define IDC_LISTVIEW                            40003
 #define IDC_SLIDER_CTRL1                        40004
 #define IDC_PROGRESS_BAR1                       40005
 #define IDC_RADO2                               40006
@@ -15,8 +15,8 @@
 #define IDC_STATIC1                             40008
 #define IDC_EDIT_CTRL1                          40009
 #define IDC_CHK_StayOnTop                       40010
-#define IDC_ADDITEM                             40011
-#define IDC_DELITEM                             40012
+#define IDC_BUTN_Listview_DelSelItem            40011
+#define IDC_BUTN_Listview_AddBoth               40012
 #define IDC_LISTBOX                             40013
 #define IDC_EDIT_AddStr                         40014
 #define IDC_EDIT_Times                          40015
@@ -24,3 +24,10 @@
 #define IDC_BUTN_ListBoxRemove                  40017
 #define IDC_BUTN_ListBoxClear                   40018
 #define IDC_TEXT_ListBoxText                    40019
+#define IDC_PIC                                 40020
+#define IDC_BUTN_Listview_AddSubItem            40021
+#define IDC_BUTN_Listview_DelAll                40022
+#define IDC_EDIT_ADD                            40023
+#define IDC_TEXT_LISTVIEW                       40025
+#define IDC_EDIT_ADDSUB                         40026
+#define IDC_BUTN_Listview_AddItem               40027
